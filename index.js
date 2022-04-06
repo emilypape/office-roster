3e83bd6935786912f8b565d1b6a3182cc0b6d79f
emily <eap6787@gmail.com>
2022-04-06 17:30:37 -0600

    create generator functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
+const fs = require('fs');
+const createTeamPage = require('./lib/generateTeamPage');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -57,6 +58,8 @@ async function buildManager() {
     const {managerName, managerId, managerEmail, officeNumber} = managerObject;
     const manager = new Manager(managerName, managerId, managerEmail, officeNumber);
     team.push(manager)
+
+    nextEmployee();
 }
 
 async function nextEmployee() {
@@ -81,9 +84,134 @@ async function nextEmployee() {
     }
 }
 
+async function buildEngineer() {
+    const engineerObject = await inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the Engineer's name?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('An engineer is required!')
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is the Engineer's id number?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('Please provide an ID')
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the Engineer's email?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('Please provide an email!')
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is the Engineer's github?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('Please provide a github!')
+            }
+        }
+    ])
+    const {engineerName, engineerId, engineerEmail, engineerGithub} = engineerObject
+    const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
+    team.push(engineer);
+
+    nextEmployee();
+}
+
+async function buildIntern() {
+    const internObject = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the Intern's name?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('An intern is required!')
+            }
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is the Intern's id number?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('Please provide an ID')
+            }
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the Intern's email?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('Please provide an email!')
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What is the Intern's schooling?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                console.log('Please include schooling information!')
+            }
+        }
+    ])
+    const {internName, internId, internEmail, internSchool} = internObject
+    const intern = new Intern(internName, internId, internEmail, internSchool)
+    
+    team.push(intern);
+
+    nextEmployee();
+}
+
 async function buildTeam() {
     await buildManager()
-    await nextEmployee()
+}
+
+function finishTeam() {
+    const createTeamHtml = createTeamPage(team);
+    const fileName = 'buildMyTeam'
+
+    writeFile(fileName, createTeamHtml);
+
+}
+
+function writeFile(fileName, data) {
+    fs.writeFile(`./dist/${fileName}.html`, data, (err) => {
+        if(err){
+            console.log(err);
+        }
+        console.log('Your team page has been built!');
+    });
 }
 
 buildTeam();
